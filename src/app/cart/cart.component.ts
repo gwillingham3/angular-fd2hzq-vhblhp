6a46e589d8cecb75b2636c18bd09fb516bb69018
gwillingham3 <gerardzon@yahoo.com>
2020-03-18 10:18:41 -0400

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -13,7 +13,12 @@ export class CartComponent implements OnInit {
   items;
   checkoutForm;
 
-  constructor( private route: ActivatedRoute, private cartService: CartService, private formBuilder: FormBuilder ) { }
+  constructor( private route: ActivatedRoute, private cartService: CartService, private formBuilder: FormBuilder ) { 
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: '',
+    })
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
